80c76fab0dc42d875249868a5ecaf073dc18dbdf
yo boy <fadil060800@gmail.com>
2022-06-11 09:12:48 +0700

    first commit

--- src/configs/database.js
+++ src/configs/database.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 module.exports = {
     multipleStatements  : true,
     host                : "sql6.freemysqlhosting.net",
-    user                : "sql6498920",
-    password            : "atwEmgTqwj",
-    database            : "sql6498920"
+    user                : "sql6499007",
+    password            : "83ymVsvL2p",
+    database            : "sql6499007"
 };
